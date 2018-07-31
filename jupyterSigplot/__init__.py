3a8b2ec9a5f2973aa0c471ccabf508962f2cd76d
mamat <mamat@lgsinnovations.com>
2018-07-31 10:03:22 -0400

    changed Postbuild

--- jupyterSigplot/__init__.py
+++ jupyterSigplot/__init__.py
@@ -1,6 +1,9 @@
 from ._version import version_info, __version__
-
+import os.path
+import sys
+import warnings
 from .example import *
+from notebook import nbextensions
 
 def _jupyter_nbextension_paths():
     return [{
@@ -9,3 +12,17 @@ def _jupyter_nbextension_paths():
         'dest': 'jupyterSigplot',
         'require': 'jupyterSigplot/extension'
     }]
+
+def prepare_js():
+    pkgdir = os.path.dirname(__file__)
+    sigplotdir = os.path.join(pkgdir, 'static')
+    nbextensions.install_nbextension(sigplotdir, user=True, overwrite=True, symlink=True)
+    display(Javascript("utils.load_extensions('sigplotjs/sigplot_ext');"))
+
+def load_ipython_extension(ipython):
+    prepare_js()
+    ipython.push("SigPlot")
+
+
+def unload_ipython_extension(ipython):
+    pass
