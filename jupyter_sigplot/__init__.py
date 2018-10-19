0e22ec29a41b9a1a72ace605e2a4674930629774
Michael Recachinas <m.recachinas@gmail.com>
2018-10-19 21:08:20 +0100

    fix: #5 version 0.1.1 had an import error
    Removed errant line

--- jupyter_sigplot/__init__.py
+++ jupyter_sigplot/__init__.py
@@ -2,7 +2,6 @@ from ._version import version_info, __version__
 import os.path
 import sys
 import warnings
-from .example import *
 from notebook import nbextensions
 
 def _jupyter_nbextension_paths():
