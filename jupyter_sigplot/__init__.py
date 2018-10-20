815b033a0e7fc1abbefdcb49bbf13e224d68e148
Michael Recachinas <mgr3yp@virginia.edu>
2018-10-20 14:25:41 +0100

    Clean up __init__.py

--- jupyter_sigplot/__init__.py
+++ jupyter_sigplot/__init__.py
@@ -1,14 +1,25 @@
+#!/usr/bin/env python
 from ._version import version_info, __version__
 import os.path
 import sys
 import warnings
 from notebook import nbextensions
 
+
 def _jupyter_nbextension_paths():
+    # type: () -> List[Dict[str, str]]
+    '''
+    Exposes the Jupyter Notebook Extension entrypoints
+    '''
     return [{
         'section': 'notebook',
+
+        # the path is relative to the `jupyter_sigplot` directory
         'src': 'static',
+
+        # directory in the `nbextension/` namespace
         'dest': 'jupyter_sigplot',
+
+        # _also_ in the `nbextension/` namespace
         'require': 'jupyter_sigplot/extension'
     }]
-
