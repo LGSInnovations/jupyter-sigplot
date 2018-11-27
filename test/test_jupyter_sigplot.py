8a227dc54977b99e95039f3402506b5ed2401a95
Michael Recachinas <mgr3yp@virginia.edu>
2018-11-27 16:34:47 +0100

    Configure flake8 to not care about a not-top-of-file import in tests

--- test/test_jupyter_sigplot.py
+++ test/test_jupyter_sigplot.py
@@ -10,7 +10,7 @@ from IPython.testing.globalipapp import get_ipython
 ip = get_ipython()
 
 from jupyter_sigplot.sigplot import SigPlot  # noqa: E402
-from testutil import EnvironmentVariable
+from testutil import EnvironmentVariable  # noqa: E402
 
 
 def test_empty_object():
