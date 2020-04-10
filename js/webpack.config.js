9be5c76ad6f609aee303de749d38b5fcef11e177
Michael Recachinas <mgr3yp@virginia.edu>
2020-04-10 12:22:59 +0100

    fix: output filename must be extension.js not nb_extension.js

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -2,10 +2,8 @@ const fs = require('fs-extra');
 const path = require('path');
 const version = require('./package.json').version;
 
-/**
- * Custom webpack rules are generally the same for all webpack bundles, hence
- * stored in a separate local variable.
- */
+// Custom webpack rules are generally the same for all webpack bundles, hence
+// stored in a separate local variable.
 const rules = [
   {test: /\.css$/, use: ["style-loader", "css-loader"]},
 ];
@@ -14,37 +12,33 @@ const rules = [
 const staticDir = path.resolve(__dirname, '..', 'jupyter_sigplot', 'static');
 
 // Copy the package.json to static so we can inspect its version.
-fs.copySync('./package.json', path.join(staticDir, 'package.json'))
+fs.copySync('./package.json', path.join(staticDir, 'package.json'));
 
 const externals = ['@jupyter-widgets/base'];
 
 module.exports = [
-  /**
-   * Notebook extension
-   *
-   * This bundle only contains the part of the JavaScript that is run on
-   * load of the notebook. This section generally only performs
-   * some configuration for requirejs, and provides the legacy
-   * `load_ipython_extension` function which is required for any notebook
-   * extension.
-   */
+  // Notebook extension
+  //
+  // This bundle only contains the part of the JavaScript that is run on
+  // load of the notebook. This section generally only performs
+  // some configuration for requirejs, and provides the legacy
+  // `load_ipython_extension` function which is required for any notebook
+  // extension.
   {
     entry: './src/nb_extension.js',
     output: {
-      filename: 'nb_extension.js',
+      filename: 'extension.js',
       path: staticDir,
       libraryTarget: 'amd'
     }
   },
 
-  /**
-   * Bundle for the notebook containing the custom widget views and models
-   *
-   * This bundle contains the implementation for the custom widget views and
-   * custom widget.
-   *
-   * Note: It must be an 'amd' module.
-   */
+  // Bundle for the notebook containing the custom widget views and models
+  //
+  // This bundle contains the implementation for the custom widget views and
+  // custom widget.
+  //
+  // Note: It must be an 'amd' module.
   {
     entry: './src/nb_index.js',
     output: {
@@ -59,21 +53,19 @@ module.exports = [
     externals: externals,
   },
 
-  /**
-   * Embeddable jupyter_sigplot bundle
-   *
-   * This bundle is generally almost identical to the notebook bundle
-   * containing the custom widget views and models.
-   *
-   * The only difference is in the configuration of the webpack public path
-   * for the static assets.
-   *
-   * It will be automatically distributed by unpkg to work with the static
-   * widget embedder.
-   *
-   * The target bundle is always `dist/index.js`, which is the path required
-   * by the custom widget embedder.
-   */
+  // Embeddable jupyter_sigplot bundle
+  //
+  // This bundle is generally almost identical to the notebook bundle
+  // containing the custom widget views and models.
+  //
+  // The only difference is in the configuration of the webpack public path
+  // for the static assets.
+  //
+  // It will be automatically distributed by unpkg to work with the static
+  // widget embedder.
+  //
+  // The target bundle is always `dist/index.js`, which is the path required
+  // by the custom widget embedder.
   {
     entry: './src/index.js',
     output: {
